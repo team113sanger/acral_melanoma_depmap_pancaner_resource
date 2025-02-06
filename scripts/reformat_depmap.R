82faec8fafce45d72b3c35c8ecefb8f8e9c5d149
jb63 <jb63@sanger.ac.uk>
2025-02-06 11:43:06 +0000

    Prep renmae

--- scripts/reformat_depmap.R
+++ scripts/reformat_depmap.R
@@ -10,11 +10,11 @@ library(glue)
 # renv::install("francescojm/CRISPRcleanR")
 library(CRISPRcleanR)
 library(data.table)
-library(depmap)
 
 ################################################################################
 # Reading in the data
 ################################################################################
+
 models <- read_csv(here("data/ScreenSequenceMap.csv")) |> 
   mutate(ModelID = case_when(!is.na(Replicate) ~ glue("{ModelID}_{Replicate}_{pDNABatch}_{ModelConditionID}"),
                              TRUE ~ SequenceID)) |> 
@@ -29,17 +29,19 @@ guide_map <- read_csv(here("data/AvanaGuideMap.csv")) |>
 
 avana_counts <- fread(here("data/AvanaLogfoldChange.csv"), fill = TRUE) 
 
-rename_map <- setNames(models$ModelID, models$SequenceID)
-
+rename_map <- setNames(models[["ModelID"]], models[["SequenceID"]])
 common_cols <- intersect(names(avana_counts), names(rename_map))
 
 
 # Rename only the matching columns
 names(avana_counts) <- c("guide", rename_map[common_cols])
+
+
 avana_counts |> 
 left_join(guide_map, by = c("guide" = "sgRNA")) |>
 relocate(Gene, .after = guide) |>
-as_tibble()
+as_tibble() |> 
+write_tsv("results/avana_lfc_rename.tsv")
     
 
 
